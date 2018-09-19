a75d1ad06b7bb8e6cb7f9b0845c0414c9611a8ea
Ryan Madigan <t00200799@365s.ittralee.ie>
2018-09-19 12:52:52 +0100

    Signed-off-by: Ryan Madigan <t00200799@365s.ittralee.ie>

---
+++ Worms 3D/Assets/ProjectileSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+    public Object projectilePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        Instantiate(projectilePrefab);
+		
+	}
+}
