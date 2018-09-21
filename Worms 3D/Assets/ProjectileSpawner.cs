d9cddd34c73f53ac4804cad5d20fedc519a62c23
Ryan Madigan <t00200799@365s.ittralee.ie>
2018-09-21 12:53:41 +0100

    Worked with projectiles and spawner, got scripts communicating
    Signed-off-by: Ryan Madigan <t00200799@365s.ittralee.ie>

--- Worms 3D/Assets/ProjectileSpawner.cs
+++ Worms 3D/Assets/ProjectileSpawner.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class ProjectileSpawner : MonoBehaviour {
 
-    public Object projectilePrefab;
+    public Object grenadePrefab;
+    public Object MissilePrefab;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        Instantiate(projectilePrefab);
-		
-	}
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GameObject newProjectileGO = (GameObject)Instantiate(grenadePrefab);
+            ProjectileControl newProjectileScript = newProjectileGO.GetComponent<ProjectileControl>();
+
+            newProjectileScript.youAreA(ProjectileControl.ProjectileType.Grenade, new Vector3(2,3,4), new Vector3(1,0,0),  5.0f );  
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            GameObject newProjectileGO = (GameObject)Instantiate(grenadePrefab);
+            ProjectileControl newProjectileScript = newProjectileGO.GetComponent<ProjectileControl>();
+
+            newProjectileScript.youAreA(ProjectileControl.ProjectileType.Missile, new Vector3(-2, 3, 4), new Vector3(0, 1, 0), 15.0f);
+        }
+
+    }
 }
