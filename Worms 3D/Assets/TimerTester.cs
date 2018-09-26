35e944127388a94f1f8264bcc35eb642a7416879
Sean Conway <t00196102@365s.ittralee.ie>
2018-09-26 12:55:55 +0100

    Timer- to be continued

---
+++ Worms 3D/Assets/TimerTester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTester : MonoBehaviour {
+    Timer ourTimer;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ourTimer = gameObject.AddComponent<Timer>();
+            ourTimer.StartTimer(0);
+        }
+
+        if (ourTimer)  print(ourTimer.current_Time);
+	}
+}
