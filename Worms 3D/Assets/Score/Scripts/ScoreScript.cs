c67b8d6f69c7b72a5ddb554137379ce531ff998c
Emma P. Fitzgerald <Emma.P.Fitzgerald@students.ittralee.ie>
2018-10-26 12:58:30 +0100

    26/10/18 made a scene that set the score Signed-off-by: Emma P. Fitzgerald <Emma.P.Fitzgerald@students.ittralee.ie>

---
+++ Worms 3D/Assets/Score/Scripts/ScoreScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+    //Initializating variables
+   int current_Score = 15;
+     // private System.String scoreText;
+    Text scoreText;
+	// Use this for initialization
+	void Start () {
+        scoreText = gameObject.GetComponentInChildren<Text>();
+        if (scoreText) print("yeee"); else print("nnnnn");
+
+        //scoreText.text = "Score \n" + current_Score.ToString();
+	}
+
+
+    public void scoreIncrease()
+    {
+        current_Score += 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "Score \n" + current_Score.ToString();
+    }
+}
