35f3a5620120cd71c022252bd63ae9e85ff14a55
T00199391 <t00199391@students.ittralee.ie>
2018-11-06 18:17:22 +0000

    Signed-off-by: T00199391 <t00199391@students.ittralee.ie>

--- Worms 3D/Assets/Score/Scripts/ScoreTest.cs
+++ Worms 3D/Assets/Score/Scripts/ScoreTest.cs
@@ -2,24 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreTest : MonoBehaviour {
+public class ScoreTest : MonoBehaviour
+{
 
     //Initial the score object
     ScoreScript ourScore;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        //Calls the ScoreScript
+        ourScore = gameObject.AddComponent<ScoreScript>();
+        //Calls the metjod to set the position of the text for the score 
+        //ourScore.setPosition(420, 270, 0);
+    }
 
+    void Update()
+    {
+        //Calls the method to set the score
+        setScore();
+        //Calls the method to set the colour of the text
+        ourScore.setTextColour(0);
+        //Calls the method to set the size of the text
+        ourScore.setFontSize(25);
+    }
+
+    //Increments the score each time you press space
+    public void setScore()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            ourScore = gameObject.AddComponent<ScoreScript>();
             ourScore.scoreIncrease();
         }
-
     }
 }
