ae5f7b1cf9a464d8746ee0c491f56d0107119df8
Sean Conway <Sean.Conway@students.ittralee.ie>
2018-11-14 12:59:58 +0000

    Error fix

--- Worms 3D/Assets/ProjectileSpawner.cs
+++ Worms 3D/Assets/ProjectileSpawner.cs
@@ -13,15 +13,15 @@ public class ProjectileSpawner : MonoBehaviour {
     //PlayerControl player;
     public UnityEngine.Object grenadePrefab;
     public UnityEngine.Object MissilePrefab;
-    PowerDisplay Strengthmeter;
+
     TimeAndDisplayCountup strengthMeter;
     private float MaxGrenadeSpeed = 40;
     AimCameraControl ourAimCam;
     GameObject crosshairs;
     WormControl ourOwner;
+    FloatingDisplay strengthMeterDisplay;
 
-    
-    
+
 
     // Use this for initialization
     void Start () {
@@ -40,7 +40,7 @@ public class ProjectileSpawner : MonoBehaviour {
                 //Checks that the player inventory has a grenade
                 //if (player.allTeams[player.current_Team_Index].teamInventory.getGrenades() > 0)
                 //{
-                if (strengthMeterDisplay)  // grenade strength being calculated
+               if (strengthMeterDisplay)  // grenade strength being calculated
                 {
                      strengthMeterDisplay.setDisplay(strengthMeter.relativePercentage().ToString());
                 
@@ -71,7 +71,7 @@ public class ProjectileSpawner : MonoBehaviour {
 
             else
             {
-                if (Strengthmeter)
+                if (strengthMeterDisplay)
                 {
                     createGrenade();
 
@@ -147,7 +147,7 @@ public class ProjectileSpawner : MonoBehaviour {
 
             Destroy(strengthMeter);
 
-            Strengthmeter.manuallyDestroy();
+            strengthMeterDisplay.manuallyDestroy();
         }
 
     
